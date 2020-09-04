62ea6ccbaa4fdbcf5868c29b4ea35d30f6100799
Eugene Sklyarenko <zhenia.sk10@gmail.com>
2020-09-04 14:09:56 +0300

    Initial commit

---
+++ TODOList/UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TODOList
+//
+//  Created by Evgeny Sklyarenko on 04.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ContentView()
+    }
+    
+}
